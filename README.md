5cd344e16429cf7fa361d2a519c40334c2e87a18
BenRaz123 <ben.raz2008@gmail.com>
2023-06-24 16:38:58 -0400

    Added Support for External Config
    Did this by creating a `Config` struct as well as some functions. Renamed `--use-kbps` to `--toggle-kpbs`. Details in `README.md`.
    
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- README.md
+++ README.md
@@ -11,6 +11,24 @@ To install, run
 % cargo install res2br
 ```
 
+### ✨ new in 2.0.0! ✨ Configuration
+
+With configuration, you can store a custom table as well you preference for using kbps with a config file. By default, `res2br` will search `~/.config/res2br/config.json` for config. However, you can change this by changing the `$RES2BR_CONFIG` environment variable or by passing in `-c <PATH>` or `--config-path <PATH>`. The json file recognizes 2 fields: `"table"` (a 'dictionary' or 'hashmap' consisting of strings and floats) as well as `"use_kbps_by_default"` (a boolean). The default configuration looks like this:
+
+```json
+{
+    "table": {
+        "1080p": 3.000,
+        "720p": 1.500,
+        "540p": 0.989,
+        "360p": 0.460,
+        "270p": 0.327,
+        "180p": 0.193
+    },
+    "use_kbps_by_default": false
+}
+```
+
 ### Status
 
 - [x]  Started
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,67 @@
 use clap::*;
+use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+   table: HashMap<String, f64>,
+   use_kbps_by_default: bool
+}
 
 #[derive(Parser, Debug)]
 #[command(name="res2br", author="Ben Raz <ben.raz2008@gmail.com>", about="Converts a resolution (like 1080p) to a bitrate (like 3.000) and optionally allows switching betwen MBPS (default) and KBPS", long_about=None)]
 struct Arguments {
     #[arg(help = "Accepts a resolution in ###p format")]
     resolution: Option<String>,
-    #[arg(short = 'k', long = "use-kbps")]
+    #[arg(short = 't', long = "toggle-kbps")]
     #[arg(action=ArgAction::SetTrue)]
-    #[arg(help = "Switches to displaying bitrate in KBPS format")]
-    use_kbps: Option<bool>,
+    #[arg(help = "Switches to and from displaying bitrate in KBPS format")]
+    toggle_kbps: Option<bool>,
+    #[arg(short, long="config-path")]
+    #[arg(help = "A path to find the config file at. If left empty, $RES2BR_CONFIG will be used. It is $HOME/.config/res2br/config.json by default. If that file is not present, the default configuration is used.")]
+    config_path: Option<String>
+}
+
+fn get_config_path(argument: &Option<String>) -> String {
+    if argument.is_some() {
+        return argument.clone().unwrap();
+    }
+    if std::env::var("RES2BR_CONFIG").is_ok() {
+        return std::env::var("RES2BR_CONFIG").unwrap();
+    }
+    std::env::set_var("RES2BR_CONFIG", "$HOME/.config/res2br/config.json");
+    "$HOME/.config/res2br/config.json".into()
+}
+
+fn get_config_from_path(path: &str) -> Result<Config, Box<dyn std::error::Error>> {
+   let file_string: String = std::fs::read_to_string(path)?.parse()?;
+   let config: Config = serde_json::from_str(&file_string)?;
+   Ok(config)
 }
 
 fn main() {
     let arguments = Arguments::parse();
 
-    let table: HashMap<String, f32> = HashMap::from([
+    let default_config: Config = Config {
+        table: HashMap::from([
         ("1080p".to_string(), 3.000),
         ("720p".to_string(), 1.500),
         ("540p".to_string(), 0.989),
         ("360p".to_string(), 0.460),
         ("270p".to_string(), 0.327),
         ("180p".to_string(), 0.193),
-    ]);
+        ]),
+        use_kbps_by_default: false
+    };
+    
+    let config_path = get_config_path(&arguments.config_path);
+    let user_config = get_config_from_path(&config_path);
+
+    let config: Config = match user_config {
+        Ok(config) => config,
+        Err(_) => default_config
+    };
 
     let show_resolution = match &arguments.resolution {
         Some(_) => false,
@@ -31,7 +70,7 @@ fn main() {
 
     if show_resolution {
         println!("Possible resolutions:");
-        for (key, _) in &table {
+        for (key, _) in &config.table {
             print!("{key} ");
         }
         println!();
@@ -40,20 +79,20 @@ fn main() {
 
     let resolution = &arguments.resolution.expect("Oops");
     
-    let is_kbps: bool = match &arguments.use_kbps {
+    let kbps_switched: bool = match &arguments.toggle_kbps {
         Some(value) => *value,
         None => false,
     };
 
-    if !table.contains_key(resolution) {
+    if !config.table.contains_key(resolution) {
         println!("\"{resolution}\" Is an invalid resolution. Try Again!");
         std::process::exit(1)
     }
 
-    if !is_kbps {
-        println!("{} Mbps", table[resolution]);
+    if !config.use_kbps_by_default && !kbps_switched {
+        println!("{} Mbps", config.table[resolution]);
         std::process::exit(0);
     }
 
-    println!("{} Kbps", table[resolution] * 1000.0);
+    println!("{} Kbps", config.table[resolution] * 1000.0); 
 }
